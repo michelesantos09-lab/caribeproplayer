5d1bd4063ac0be0ee04fcad49cd9bea01b589282
michelesantos09-lab <michele.santos09@escola.pr.gov.br>
2026-05-27 11:34:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.body;
+body.style.margin= '0';
+
+//========= CABEÇALHO =========
+const cabecalho = document.createElement ('h1');
+cabecalho.textContent = 'criando elementos com javaScript';
